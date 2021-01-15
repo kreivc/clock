5ede9b81a1820b185ef1019219064f5abdd9ec02
Ricky <phangr28@gmail.com>
2021-01-15 11:25:16 +0700

    add lazy loader

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
+import React, { Suspense } from "react";
 import "./App.css";
-import Clock from "./component/clock";
+import Loader from "./component/loader";
+const Clock = React.lazy(() => import("./component/clock"));
 
 function App() {
   const date = new Date();
   if (date.getHours() < 12) {
     return (
       <div className="AppPagi" id="app">
-        <Clock />
+        <Suspense fallback={<Loader />}>
+          <Clock />
+        </Suspense>
       </div>
     );
   } else if (date.getHours() >= 12 && date.getHours() < 18) {
     return (
       <div className="AppSiang" id="app">
-        <Clock />
+        <Suspense fallback={<Loader />}>
+          <Clock />
+        </Suspense>
       </div>
     );
   } else {
     return (
       <div className="AppMalam" id="app">
-        <Clock />
+        <Suspense fallback={<Loader />}>
+          <Clock />
+        </Suspense>
       </div>
     );
   }
---
+++ src/component/loader.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./loader.css";
+
+function Loader() {
+  return (
+    <div>
+      <h1 className="loading">Loading...</h1>
+      <h2 className="loading2">Getting Date & Time..</h2>
+    </div>
+  );
+}
+
+export default Loader;
