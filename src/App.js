943c82261191bb05aa79cf0c9408950f13c0c5dd
Ricky <phangr28@gmail.com>
2021-01-16 17:46:18 +0700

    add input but still cannot post and get

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React, { Suspense } from "react";
+import React, { Suspense, useRef, useState } from "react";
 import "./App.css";
 import Loader from "./component/loader";
 
 import firebase from "firebase/app";
 import "firebase/firestore";
 import "firebase/auth";
-import "firebase/analytics";
+// import "firebase/analytics";
 
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useCollectionData } from "react-firebase-hooks/firestore";
@@ -24,7 +24,7 @@ firebase.initializeApp({
 
 const auth = firebase.auth();
 const firestore = firebase.firestore();
-const analytics = firebase.analytics();
+// const analytics = firebase.analytics();
 
 function App() {
   const [user] = useAuthState(auth);
@@ -64,7 +64,10 @@ function App() {
       </header>
       <section>
         {user ? (
-          <ClockFinal />
+          <div>
+            <Alarm />
+            <ClockFinal />
+          </div>
         ) : (
           <div>
             <SignIn />
@@ -74,31 +77,90 @@ function App() {
       </section>
     </div>
   );
+}
 
-  function SignIn() {
-    const signInWithGoogle = () => {
-      const provider = new firebase.auth.GoogleAuthProvider();
-      auth.signInWithPopup(provider);
-    };
+function SignIn() {
+  const signInWithGoogle = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider);
+  };
 
-    return (
-      <>
-        <button className="sign" onClick={signInWithGoogle}>
-          Sign in with Google
+  return (
+    <>
+      <button className="sign" onClick={signInWithGoogle}>
+        Sign in with Google
+      </button>
+    </>
+  );
+}
+
+function SignOut() {
+  return (
+    auth.currentUser && (
+      <button className="sign" onClick={() => auth.signOut()}>
+        Sign Out
+      </button>
+    )
+  );
+}
+
+function Alarm() {
+  const dummy = useRef();
+  const alarmRef = firestore.collection("alarm");
+  const query = alarmRef.orderBy("detail").limit(25);
+
+  const [alarm] = useCollectionData(query, { idField: "id" });
+  const [formValue, setFormValue] = useState("");
+
+  const sendAlarm = async (e) => {
+    e.preventDefault();
+
+    const { uid, displayName } = auth.currentUser;
+
+    await alarmRef.add({
+      detail: formValue,
+      displayName,
+      uid,
+    });
+
+    setFormValue("");
+    dummy.current.scrollIntoView({ behavior: "smooth" });
+  };
+  return (
+    <>
+      <form className="sectionInput" onSubmit={sendAlarm}>
+        <input
+          className="inpTime"
+          value={formValue}
+          onChange={(e) => setFormValue(e.target.value)}
+          placeholder="Input Alarm Time"
+        />
+
+        <button className="submitTime" type="submit" disabled={!formValue}>
+          Submit
         </button>
-      </>
-    );
-  }
 
-  function SignOut() {
-    return (
-      auth.currentUser && (
-        <button className="sign" onClick={() => auth.signOut()}>
-          Sign Out
-        </button>
-      )
-    );
-  }
+        <div className="alrView">
+          {alarm && alarm.map((alr) => <AlarmView key={alr.id} alarm={alr} />)}
+          <span ref={dummy}></span>
+        </div>
+      </form>
+    </>
+  );
+}
+
+function AlarmView(props) {
+  const { detail, uid } = props.alarm;
+
+  const detailClass = uid === auth.currentUser.uid ? "on" : "off";
+
+  return (
+    <>
+      <div className={`alarm ${detailClass}`}>
+        <h1>{detail}</h1>
+      </div>
+    </>
+  );
 }
 
 export default App;
