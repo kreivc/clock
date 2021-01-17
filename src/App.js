241841186e80dbf280962a33289f1f25b48dcb0d
Ricky <phangr28@gmail.com>
2021-01-17 22:24:08 +0700

    add selection minor changes

--- src/App.js
+++ src/App.js
@@ -99,7 +99,8 @@ function Alarm() {
   const alarmRef = firestore.collection("alarm");
   const query = alarmRef.orderBy("detail").limit(25);
   const [alarm] = useCollectionData(query, { idField: "id" });
-  const [formValue, setFormValue] = useState("");
+  // const [formValue, setFormValue] = useState("");
+  const [alarmValue, setAlarmValue] = useState("");
 
   const sendAlarm = async (e) => {
     e.preventDefault();
@@ -107,18 +108,28 @@ function Alarm() {
     const { uid, displayName } = auth.currentUser;
 
     await alarmRef.add({
-      detail: formValue,
+      detail: alarmValue,
       displayName,
       uid,
     });
 
-    setFormValue("");
+    // setFormValue("");
+    setAlarmValue("");
     dummy.current.scrollIntoView({ behavior: "smooth" });
   };
 
   const stopAlarm = (e) => {
     e.preventDefault();
     document.querySelector(".Sound").pause();
+    document.querySelector(".Sound").currentTime = 0;
+  };
+
+  const setAlarm = () => {
+    var h = document.querySelector(".hours").value;
+    var m = document.querySelector(".minutes").value;
+    var ap = document.querySelector(".ampm").value;
+    var hmap = `${h}:${m}:00 ${ap}`;
+    setAlarmValue(hmap);
   };
 
   return (
@@ -134,24 +145,102 @@ function Alarm() {
           <span ref={dummy}></span>
         </div>
 
-        <input
+        {/* <input
           className="inpTime"
           value={formValue}
           onChange={(e) => setFormValue(e.target.value)}
           placeholder="Input Alarm Time"
-        />
+        /> */}
 
-        <button
-          className="submitTime"
-          type="submit"
-          onClick={sendAlarm}
-          disabled={!formValue}
-        >
-          Submit
-        </button>
-        <button className="stop" onClick={stopAlarm}>
-          Stop
-        </button>
+        <div>
+          <select className="hours" onChange={setAlarm}>
+            <option>00</option>
+            <option>1</option>
+            <option>2</option>
+            <option>3</option>
+            <option>4</option>
+            <option>5</option>
+            <option>6</option>
+            <option>7</option>
+            <option>8</option>
+            <option>9</option>
+            <option>10</option>
+            <option>11</option>
+            <option>12</option>
+          </select>
+          <select className="minutes" onChange={setAlarm}>
+            <option>00</option>
+            <option>01</option>
+            <option>02</option>
+            <option>03</option>
+            <option>04</option>
+            <option>05</option>
+            <option>06</option>
+            <option>07</option>
+            <option>08</option>
+            <option>09</option>
+            <option>10</option>
+            <option>11</option>
+            <option>12</option>
+            <option>13</option>
+            <option>14</option>
+            <option>15</option>
+            <option>16</option>
+            <option>17</option>
+            <option>18</option>
+            <option>19</option>
+            <option>20</option>
+            <option>21</option>
+            <option>22</option>
+            <option>23</option>
+            <option>24</option>
+            <option>25</option>
+            <option>26</option>
+            <option>27</option>
+            <option>28</option>
+            <option>29</option>
+            <option>30</option>
+            <option>31</option>
+            <option>32</option>
+            <option>33</option>
+            <option>34</option>
+            <option>35</option>
+            <option>36</option>
+            <option>37</option>
+            <option>38</option>
+            <option>39</option>
+            <option>40</option>
+            <option>41</option>
+            <option>42</option>
+            <option>43</option>
+            <option>44</option>
+            <option>45</option>
+            <option>46</option>
+            <option>47</option>
+            <option>48</option>
+            <option>49</option>
+            <option>50</option>
+            <option>51</option>
+            <option>52</option>
+            <option>53</option>
+            <option>54</option>
+            <option>55</option>
+            <option>56</option>
+            <option>57</option>
+            <option>58</option>
+            <option>59</option>
+          </select>
+          <select className="ampm" onChange={setAlarm}>
+            <option>AM</option>
+            <option>PM</option>
+          </select>
+          <button className="submitTime" type="submit" onClick={sendAlarm}>
+            Set Alarm
+          </button>
+          <button className="stop" onClick={stopAlarm}>
+            Stop
+          </button>
+        </div>
       </form>
     </>
   );
@@ -159,13 +248,13 @@ function Alarm() {
 
 function AlarmView(props) {
   const { detail } = props.alarm;
+  const [timeNow, setTimeNow] = useState();
 
   const delAlarm = async (e) => {
     e.preventDefault();
 
     firestore.collection("alarm").doc(props.alarm.id).delete();
   };
-  const [timeNow, setTimeNow] = useState();
 
   const UpdateTime = () => {
     let time = new Date().toLocaleTimeString();
@@ -176,7 +265,6 @@ function AlarmView(props) {
 
   useEffect(() => {
     if (detail === timeNow) {
-      // alert("ALARM RINGING");
       document.querySelector(".Sound").play();
     }
   }, [UpdateTime]);
